708d4c83c2c0369968aa6f18213ac37e5518a0d3
hippolyte42 <hippolytejacque@hotmail.fr>
2022-06-04 19:07:03 +0200

    deploy script

--- deploy.js
+++ deploy.js
@@ -1,1 +1,22 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  process.env.MNEMETIC,
+  process.env.INFURIA_ENDPOINT
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["gm"] })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
