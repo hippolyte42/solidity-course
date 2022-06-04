0466416aa8af1185367ac68920fd18884d292eca
hippolyte42 <hippolytejacque@hotmail.fr>
2022-06-04 19:43:47 +0200

    change repo structure - new node project `inbox-updated`

---
+++ inbox/contracts/Inbox.sol
@@ -0,0 +1,14 @@
+// linter warnings (red underline) about pragma version can be igonored!
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
