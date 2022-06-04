219eca2f76cd0b7e5100f60d73e4a5213ab2695c
hippolyte42 <hippolytejacque@hotmail.fr>
2022-06-04 19:51:18 +0200

    update code for `inbox-updated` - ready to start section 3

--- inbox-updated/test/Inbox.test.js
+++ inbox-updated/test/Inbox.test.js
@@ -1,23 +1,20 @@
-/* eslint-disable no-undef */
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
+
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
-const INITIAL_MESSAGE = "wagmi";
 
 beforeEach(async () => {
-  // get a list of all accounts
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
-      arguments: [INITIAL_MESSAGE],
+      data: evm.bytecode.object,
+      arguments: ["Hi there!"],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
@@ -26,18 +23,13 @@ describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
   });
-
   it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, INITIAL_MESSAGE);
+    assert.equal(message, "Hi there!");
   });
-
   it("can change the message", async () => {
-    const newMessage = "gm";
-    await inbox.methods
-      .setMessage(newMessage)
-      .send({ from: accounts[0], gas: "1000000" });
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
-    assert.equal(message, newMessage);
+    assert.equal(message, "bye");
   });
 });
