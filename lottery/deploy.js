0388067363367a8be72eba0873065f95f9a01582
hippolyte42 <hippolytejacque@hotmail.fr>
2022-06-05 21:46:58 +0200

    Lottery smart contract + tests

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -13,7 +13,7 @@ const deploy = async () => {
   console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["gm"] })
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 
   console.log("Contract deployed to", result.options.address);
